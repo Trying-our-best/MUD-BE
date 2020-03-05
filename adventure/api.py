aa36d812e99a0c4dac600a39f0300372ec6a1548
jambis <imjaamees@gmail.com>
2020-03-05 16:39:38 -0600

    Update api to give you x and y coordinates

--- adventure/api.py
+++ adventure/api.py
@@ -20,7 +20,7 @@ def initialize(request):
     uuid = player.uuid
     room = player.room()
     players = room.playerNames(player_id)
-    return JsonResponse({'uuid': uuid, 'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players}, safe=True)
+    return JsonResponse({'uuid': uuid, 'name':player.user.username, 'title':room.title, 'description':room.description, "x": room.coordinateX, "y": room.coordinateY, 'players':players}, safe=True)
 
 @csrf_exempt
 @api_view(["GET"])
@@ -124,10 +124,10 @@ def move(request):
         #     pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has walked {dirs[direction]}.'})
         # for p_uuid in nextPlayerUUIDs:
         #     pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has entered from the {reverse_dirs[direction]}.'})
-        return JsonResponse({'name':player.user.username, 'id': nextRoom.id, 'title':nextRoom.title, 'description':nextRoom.description, 'players':players, 'error_msg':""}, safe=True)
+        return JsonResponse({'name':player.user.username, 'id': nextRoom.id, 'title':nextRoom.title, 'description':nextRoom.description, "x": room.coordinateX, "y": room.coordinateY, 'players':players, 'error_msg':""}, safe=True)
     else:
         players = room.playerNames(player_id)
-        return JsonResponse({'name':player.user.username, 'id': room.id, 'title':room.title, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
+        return JsonResponse({'name':player.user.username, 'id': room.id, 'title':room.title, 'description':room.description, "x": room.coordinateX, "y": room.coordinateY, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
 
 
 @csrf_exempt
